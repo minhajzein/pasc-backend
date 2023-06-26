80e015264e42cedc6ad8de2328c91a841d8f09c3
minhajzein <109206665+minhajzein@users.noreply.github.com>
2023-06-26 12:31:16 +0530

    cors policy changed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
-    'https://www.pasc.site',
-    'https://pasc.site'
+    // 'http://127.0.0.1:5173',
+    // 'https://www.pasc.site',
+    // 'https://pasc.site',
+    'https://pasc-frontend.vercel.app'
 ]
 
 module.exports = allowedOrigins
