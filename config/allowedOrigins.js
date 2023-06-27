728a15d369b8ba8e0283ef6fd714a70bb0180bc3
minhajzein <109206665+minhajzein@users.noreply.github.com>
2023-06-27 13:24:42 +0530

    .env removed

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
-    // 'http://127.0.0.1:5173',
-    // 'https://www.pasc.site',
-    // 'https://pasc.site',
+    'http://127.0.0.1:5173',
+    'https://www.pasc.site',
+    'https://pasc.site',
     'https://pasc-frontend.vercel.app'
 ]
 
