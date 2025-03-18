573f0e7742bc110efd6026a046a8883a7c912e62
minhajgrohance <minhaj.grohance@gmail.com>
2025-03-18 11:03:34 +0530

    existing mobile and email validated

--- controller/client/userController.js
+++ controller/client/userController.js
@@ -39,6 +39,13 @@ module.exports = {
 
     updateProfile: async (req, res) => {
         try {
+            const userWithEmail = await User.find({ email: req.body.email })
+            const userwithMobile = await User.find({ mobile: req.body.mobile })
+            if (userWithEmail[0] !== undefined)
+                return res.status(200).send({ message: 'Email is already registered', success: false })
+            if (userwithMobile[0] !== undefined)
+                return res.status(200).send({ message: 'Mobile Number is already registered', success: false })
+
             const user = await User.findByIdAndUpdate(req.params.id, {
                 email: req.body.email,
                 mobile: req.body.mobile,
